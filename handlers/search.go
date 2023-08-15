56fbd9c9a040ec270e586337be3cb140eab13787
Dillon McQuade <dillonkmcquade@gmail.com>
2023-08-15 18:53:44 -0400

    search handler

---
+++ handlers/search.go
@@ -0,0 +1,63 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Ticker struct {
+	Symbol      string `json:"symbol"`
+	Description string `json:"description"`
+}
+
+type SearchResponse struct {
+	Status int      `json:"status"`
+	Data   []Ticker `json:"data"`
+}
+
+func Search(rw http.ResponseWriter, req *http.Request, pool *pgxpool.Pool) {
+	name := req.URL.Query().Get("name")
+	if name == "" {
+		http.Error(rw, "No search query given", http.StatusBadRequest)
+		return
+	}
+	ctx := req.Context()
+	rows, err := pool.Query(ctx, `
+        SELECT *
+        FROM tickers
+        WHERE description LIKE $1 || '%'
+        LIMIT 10
+    `, name)
+	defer rows.Close()
+	if err != nil {
+		http.Error(rw, "Error retrieving resource from database", http.StatusInternalServerError)
+		fmt.Println(err)
+		return
+	}
+	var results []Ticker
+	for rows.Next() {
+		var ticker Ticker
+		if err = rows.Scan(&ticker.Symbol, &ticker.Description); err != nil {
+			log.Println(err)
+		}
+		results = append(results, ticker)
+	}
+	if err = rows.Err(); err != nil {
+		log.Println(err)
+	}
+	if len(results) == 0 {
+		http.Error(rw, "No results", http.StatusBadRequest)
+		return
+	}
+
+	encoder := json.NewEncoder(rw)
+
+	err = encoder.Encode(SearchResponse{Data: results, Status: 200})
+	if err != nil {
+		log.Println(err)
+	}
+}
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"goServer/handlers"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+func connectToDatabase(database_uri string) *pgxpool.Pool {
+	// DB
+	dbpool, err := pgxpool.New(context.Background(), database_uri)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Connected to Database")
+	return dbpool
+}
+
+func main() {
+	// logger
+	l := log.New(os.Stdout, "Api", log.LstdFlags)
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("Missing environment variables")
+		os.Exit(1)
+		return
+	}
+	database_uri := os.Getenv("DATABASE_URL")
+	dbpool := connectToDatabase(database_uri)
+	defer dbpool.Close()
+
+	// Router setup
+	sm := chi.NewRouter()
+	sm.Use(middleware.Logger)
+	sm.Get("/search", func(rw http.ResponseWriter, request *http.Request) {
+		handlers.Search(rw, request, dbpool)
+	})
+
+	// server opts
+	server := http.Server{
+		Addr:         ":3001",
+		Handler:      sm,
+		ErrorLog:     l,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	// non blocking server
+	go func() {
+		fmt.Printf("Listening on port %s\n", server.Addr)
+
+		err := server.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+	// Notify on Interrupt/kill
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+	sig := <-sigChan
+	l.Println("Graceful shutdown", sig)
+
+	// graceful shutdown
+	tc, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	server.Shutdown(tc)
+}
